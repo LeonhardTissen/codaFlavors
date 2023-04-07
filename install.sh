904679713f7e7ed87ee37549a21b2cfdb5a13269
Warze <contact@warze.org>
2023-04-07 20:27:48 +0200

    Removed install scripts from repo

--- install.sh
+++
@@ -1,5 +0,0 @@
-rm -R ~/.local/share/NecroDancer/mods/CodaFlavors/sprites
-rm -R ~/.local/share/NecroDancer/mods/CodaFlavors/scripts
-
-cp -R ~/GitHub/CodaFlavors/scripts ~/.local/share/NecroDancer/mods/CodaFlavors
-cp -R ~/GitHub/CodaFlavors/sprites ~/.local/share/NecroDancer/mods/CodaFlavors
