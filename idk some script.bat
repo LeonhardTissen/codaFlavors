7eb8939cba45688b11d1603fd28758ff7e7928f1
Warze <contact@warze.org>
2023-04-12 02:04:28 +0200

    Delete idk some script.bat

--- idk some script.bat
+++
@@ -1,7 +0,0 @@
-rmdir "C:/Users/Admin/AppData/Local/NecroDancer/mods/CodaFlavors/scripts" /s/q
-rmdir "C:/Users/Admin/AppData/Local/NecroDancer/mods/CodaFlavors/sprites" /s/q
-rmdir "C:/Users/Admin/AppData/Local/NecroDancer/mods/CodaFlavors/logo" /s/q
-
-xcopy "C:/Users/Admin/Desktop/GitHub/CodaFlavors/scripts" "C:/Users/Admin/AppData/Local/NecroDancer/mods/CodaFlavors/scripts" /E/C/I 
-xcopy "C:/Users/Admin/Desktop/GitHub/CodaFlavors/sprites" "C:/Users/Admin/AppData/Local/NecroDancer/mods/CodaFlavors/sprites" /E/C/I 
-xcopy "C:/Users/Admin/Desktop/GitHub/CodaFlavors/logo" "C:/Users/Admin/AppData/Local/NecroDancer/mods/CodaFlavors/logo" /E/C/I 
